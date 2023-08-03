76129dd14514340c0882dc3542dd1efd09d77062
MorskoySom <Somovvitvit@gmail.com>
2023-08-03 13:24:46 +0300

    1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,19 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
 console.log(galleryItems);
+
+const container = document.querySelector(`.gallery`);
+
+function createMarkup(arr) {
+    return arr.map(({preview, original, description}) => `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`)
+}
+createMarkup(galleryItems);
